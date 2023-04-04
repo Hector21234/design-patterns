d2717f6fe571ea2313923d1e03516d5000348b52
Hector21234 <healgo@floridauniversitaria.es>
2023-04-04 10:54:08 +0200

    commit ejercicio

---
+++ editor/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author falbo
+ *
+ */
+module pets {
+}
---
+++ pets/src/main/java/com/kreitek/pets/domain/Debug.java
@@ -0,0 +1,27 @@
+
+package com.kreitek.pets.domain;
+import java.util.Scanner;
+
+
+
+public class Debug {
+	
+	private static String debug;
+	
+	
+	
+	public void debug(String string) {
+	
+		System.out.print("[debug]");
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Debug debug1=new Debug();
+		debug1.debug(debug);
+	}
+	
+
+}
